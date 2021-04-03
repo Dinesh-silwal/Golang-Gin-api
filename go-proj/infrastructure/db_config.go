a1002a66b636c70045a0e2e7e0fb30a09be71f41
Dinesh-Silwal <dnesrnd2021@gmail.com>
2021-04-03 20:31:11 +0545

    create database configuration and load data from env file: add gotodoenv package

--- go-proj/infrastructure/db_config.go
+++ go-proj/infrastructure/db_config.go
@@ -1,1 +1,29 @@
 package infrastructure
+
+import (
+	"fmt"
+	"go-proj/go-proj/utils"
+
+	// go-sql-driver/mysql
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+// SetupModels : initializing mysql database
+func SetupModels() *gorm.DB {
+	get := utils.GetEnvWithKey
+	USER := get("DB_USER")
+	PASS := get("DB_PASS")
+	HOST := get("DB_HOST")
+	PORT := get("DB_PORT")
+	DBNAME := get("DB_NAME")
+
+	URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
+	db, err := gorm.Open("mysql", URL)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
